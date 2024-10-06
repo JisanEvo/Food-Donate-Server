49c9b1afc200e81cea79e55f6b4818262903357d
Jisan Evo~ <jesanmahmud6@gmail.com>
2024-10-06 14:19:02 +0600

    deploy sucess

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ res.send(result)
 
 
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
